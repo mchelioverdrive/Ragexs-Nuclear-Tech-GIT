50fa2b9192446e5ed1b9be9e898afcb02f859522
RagexPrince683 <yellowtoad91@gmail.com>
2025-03-22 02:43:48 -0400

    erm aktually its *sometimes* cobalt
    also where the fuck is magnesium and manganese and all that other shit in the earth

--- src/main/java/com/hbm/inventory/recipes/CrucibleRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/CrucibleRecipes.java
@@ -84,6 +84,10 @@ public class CrucibleRecipes extends SerializableRecipe {
 			.inputs(new MaterialStack(Mats.MAT_STEEL, n * 5), new MaterialStack(Mats.MAT_TUNGSTEN, n * 3), new MaterialStack(Mats.MAT_COBALT, n * 1))
 			.outputs(new MaterialStack(Mats.MAT_DURA, n * 9)));
 
+		//recipes.add(new CrucibleRecipe(3, "crucible.hss", 9, new ItemStack(ModItems.ingot_dura_steel))
+		//	.inputs(new MaterialStack(Mats.MAT_STEEL, n * 5), new MaterialStack(Mats.MAT_TUNGSTEN, n * 3), new MaterialStack(Mats.MAT_TUNGSTEN, n * 1))
+		//	.outputs(new MaterialStack(Mats.MAT_DURA, n * 9)));
+
 		recipes.add(new CrucibleRecipe(14, "crucible.hsss", 12, new ItemStack(ModItems.ingot_dura_steel))
 			.inputs(new MaterialStack(Mats.MAT_STAINLESS, n * 5), new MaterialStack(Mats.MAT_TUNGSTEN, n * 3), new MaterialStack(Mats.MAT_COBALT, n * 1))
 			.outputs(new MaterialStack(Mats.MAT_DURA, i * 2)));
