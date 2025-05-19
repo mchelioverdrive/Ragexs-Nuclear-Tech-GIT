aa6b5669905ee5252af42142e4051b368e4e9802
RagexPrince683 <yellowtoad91@gmail.com>
2025-05-19 05:04:27 -0400

    how about you use normal fucking RGB values what the fuck is this shit

--- src/main/java/com/hbm/dim/SolarSystem.java
+++ src/main/java/com/hbm/dim/SolarSystem.java
@@ -177,7 +177,10 @@ public class SolarSystem {
 					//skibidi mode: on
 					.withSemiMajorAxis(778_479_000)
 					.withRotationalPeriod(36_000)
-					.withColor(0.4588f, 0.6784f, 0.3059f)
+					//.withColor(0.4588f, 0.6784f, 0.3059f)
+					//neptune/uranus color
+					.withColor(1.0f, 0.5f, 0.0f)
+					//hopefully orange?
 					.withSatellites(
 
 						//europa
