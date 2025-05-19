bb9ad1b4d721021903fca7bd4e718832dc00d58d
RagexPrince683 <yellowtoad91@gmail.com>
2025-05-19 00:28:40 -0400

    rads on mercury and some ore tweaks

--- src/main/java/com/hbm/dim/moho/WorldProviderMoho.java
+++ src/main/java/com/hbm/dim/moho/WorldProviderMoho.java
@@ -6,13 +6,18 @@ import com.hbm.dim.WorldChunkManagerCelestial.BiomeGenLayers;
 import com.hbm.dim.WorldProviderCelestial;
 import com.hbm.dim.moho.genlayer.GenLayerMohoBiomes;
 
+import com.hbm.potion.HbmPotion;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.layer.GenLayer;
 import net.minecraft.world.gen.layer.GenLayerFuzzyZoom;
 import net.minecraft.world.gen.layer.GenLayerSmooth;
 import net.minecraft.world.gen.layer.GenLayerVoronoiZoom;
 import net.minecraft.world.gen.layer.GenLayerZoom;
+
+import java.util.Random;
 
 public class WorldProviderMoho extends WorldProviderCelestial {
 
@@ -22,10 +27,43 @@ public class WorldProviderMoho extends WorldProviderCelestial {
 	}
 
 	@Override
+	public void updateWeather() {
+		super.updateWeather();
+
+		// Apply radiation effect to players on mercury
+		if (!worldObj.isRemote) {
+			Random rand = new Random();
+
+			//todo summon a really wholesome creature here bc it's such a NICE environment.
+
+			for (Object obj : worldObj.playerEntities) {
+				if (obj instanceof EntityPlayer) {
+					EntityPlayer player = (EntityPlayer) obj;
+
+					// Check if the player can see the sky
+					if (worldObj.canBlockSeeTheSky((int) player.posX, (int) player.posY, (int) player.posZ)) {
+						// Apply radiation effect with a random chance
+						if (isDaytime()) {
+							if (rand.nextInt(15) == 0) {
+								player.addPotionEffect(new PotionEffect(HbmPotion.radiation.id, 20, 1));
+							}
+						} else {
+							if (rand.nextInt(20) == 0) {
+								player.addPotionEffect(new PotionEffect(HbmPotion.radiation.id, 20, 1));
+							}
+						}
+
+					}
+				}
+			}
+		}
+	}
+
+	@Override
 	public String getDimensionName() {
 		return "Moho";
 	}
-	
+
 	@Override
 	public IChunkProvider createChunkGenerator() {
 		return new ChunkProviderMoho(this.worldObj, this.getSeed(), false);
@@ -45,7 +83,7 @@ public class WorldProviderMoho extends WorldProviderCelestial {
 		// biomes = new GenLayerZoom(1003L, biomes);
 		biomes = new GenLayerSmooth(700L, biomes);
 		biomes = new GenLayerZoom(1006L, biomes);
-		
+
 		GenLayer genLayerVoronoiZoom = new GenLayerVoronoiZoom(10L, biomes);
 
 		return new BiomeGenLayers(biomes, genLayerVoronoiZoom, seed);
