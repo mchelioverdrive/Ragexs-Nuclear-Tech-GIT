af454c1aa71791fc8741a1b536c3d00112344020
RagexPrince683 <yellowtoad91@gmail.com>
2025-11-06 07:21:32 -0500

    phobos ores

--- src/main/java/com/hbm/dim/Ike/WorldGeneratorIke.java
+++ src/main/java/com/hbm/dim/Ike/WorldGeneratorIke.java
@@ -9,6 +9,7 @@ import com.hbm.dim.CelestialBody;
 import com.hbm.world.generator.DungeonToolbox;
 
 import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 
@@ -24,16 +25,26 @@ public class WorldGeneratorIke implements IWorldGenerator {
 	private void generateIke(World world, Random rand, int i, int j) {
 		int meta = CelestialBody.getMeta(world);
 
+		//known:
+
+		//carbon
+		DungeonToolbox.generateOre(world, rand, i, j, 16, 10, 20, 0, ModBlocks.block_graphite, 0, ModBlocks.ike_stone);
+		//clay
+		DungeonToolbox.generateOre(world, rand, i, j, 20, 12, 32, 0, Blocks.clay, 0, ModBlocks.ike_stone);
+		//silicon (silicate)
+		DungeonToolbox.generateOre(world, rand, i, j, 18, 10, 28, 0, ModBlocks.ore_silicon, 0, ModBlocks.ike_stone);
+
+		//pre existing
 		DungeonToolbox.generateOre(world, rand, i, j, WorldConfig.asbestosSpawn, 8, 3, 22, ModBlocks.ore_asbestos, meta, ModBlocks.ike_stone);
 		DungeonToolbox.generateOre(world, rand, i, j, WorldConfig.copperSpawn, 9, 4, 27, ModBlocks.ore_copper, meta, ModBlocks.ike_stone);
 		DungeonToolbox.generateOre(world, rand, i, j, WorldConfig.ironClusterSpawn,  8, 1, 33, ModBlocks.ore_iron, meta, ModBlocks.ike_stone);
 		DungeonToolbox.generateOre(world, rand, i, j, WorldConfig.lithiumSpawn,  6, 4, 8, ModBlocks.ore_lithium, meta, ModBlocks.ike_stone);
 		DungeonToolbox.generateOre(world, rand, i, j, 2, 4, 15, 40, ModBlocks.ore_coltan, meta, ModBlocks.ike_stone);
-		
+
 		//okay okay okay, lets say on duna you DO make solvent, this is now awesome because you can now make gallium arsenide to then head to
 		//dres and the likes :)
-	
-		
+
+
         DungeonToolbox.generateOre(world, rand, i, j, WorldConfig.mineralSpawn, 10, 12, 32, ModBlocks.ore_mineral, meta, ModBlocks.moho_stone);
 
 	}
