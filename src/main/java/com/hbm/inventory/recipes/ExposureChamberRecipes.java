cc66e06b8e50d4ce65e68fb104e48a850ac63dec
RagexPrince683 <yellowtoad91@gmail.com>
2026-04-30 11:26:25 -0400

    whats the point of the exposure chamber

--- src/main/java/com/hbm/inventory/recipes/ExposureChamberRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/ExposureChamberRecipes.java
@@ -15,6 +15,9 @@ import com.hbm.inventory.RecipesCommon.OreDictStack;
 import com.hbm.inventory.recipes.loader.SerializableRecipe;
 import com.hbm.items.ModItems;
 
+import com.hbm.items.machine.ItemBreedingRod;
+import com.hbm.items.machine.ItemCircuit;
+import com.hbm.items.machine.ItemZirnoxRod;
 import net.minecraft.item.ItemStack;
 
 public class ExposureChamberRecipes extends SerializableRecipe {
@@ -23,10 +26,42 @@ public class ExposureChamberRecipes extends SerializableRecipe {
 
 	@Override
 	public void registerDefaults() {
-		recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_higgs), new OreDictStack(U.ingot()), new ItemStack(ModItems.ingot_schraranium)));
-		recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_higgs), new OreDictStack(U238.ingot()), new ItemStack(ModItems.ingot_schrabidium)));
+		//recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_higgs), new OreDictStack(U.ingot()), new ItemStack(ModItems.ingot_schraranium)));
+		//recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_higgs), new OreDictStack(U238.ingot()), new ItemStack(ModItems.ingot_schrabidium)));
+		//die; oh this machine no longer has a use. Great so what are exposure chambers used for in real life?
 		//recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_dark), new OreDictStack(PU.ingot()), new ItemStack(ModItems.ingot_euphemium)));
-		recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_sparkticle), new OreDictStack(SBD.ingot()), new ItemStack(ModItems.ingot_dineutronium)));
+		//recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_sparkticle), new OreDictStack(SBD.ingot()), new ItemStack(ModItems.ingot_dineutronium)));
+
+		//item.particle_aelectron.name=Positron Capsule
+		//item.particle_amat.name=Antimatter Capsule
+		//item.particle_aproton.name=Antiproton Capsule
+		//item.particle_aschrab.name=Anti Unbihexium Capsule
+		//item.particle_copper.name=Copper Ion Capsule
+		//item.particle_dark.name=Dark Matter Capsule
+		//item.particle_digamma.name=§cThe Digamma Particle§r
+		//item.particle_empty.name=Empty Particle Capsule
+		//item.particle_higgs.name=Higgs Boson Capsule
+		//item.particle_hydrogen.name=Hydrogen Ion Capsule
+		//item.particle_lead.name=Lead Ion Capsule
+		//item.particle_lutece.name=Lutece Quasiparticle
+		//item.particle_muon.name=Muon Capsule
+		//item.particle_sparkticle.name=Sparkticle Capsule
+		//item.particle_strange.name=Strange Quark Capsule
+		//item.particle_tachyon.name=Tachyon Capsule
+
+		//particle_neutron + cobalt_ingot → cobalt_60_ingot
+		recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_aelectron), new OreDictStack(CO.ingot()), new ItemStack(ModItems.ingot_co60)));
+
+		//particle_neutron + uranium_ingot → enriched_uranium
+		recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_aelectron), new OreDictStack(U.ingot()), new ItemStack(ModItems.ingot_uranium_fuel)));
+
+		//Semiconductor doping
+		recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_copper), new OreDictStack(SI.nugget()),
+											  new ComparableStack(ModItems.circuit, 8, ItemCircuit.EnumCircuitType.CHIP_BISMOID).toStack()));
+
+		//particle_neutron + lithium → tritium
+		recipes.add(new ExposureChamberRecipe(new ComparableStack(ModItems.particle_aelectron), new ComparableStack(new ItemStack(ModItems.rod_quad, 1, ItemBreedingRod.BreedingRodType.LITHIUM.ordinal())), new ItemStack(ModItems.rod_quad, 1, ItemBreedingRod.BreedingRodType.TRITIUM.ordinal())));
+
 	}
 
 	public static ExposureChamberRecipe getRecipe(ItemStack particle, ItemStack input) {
