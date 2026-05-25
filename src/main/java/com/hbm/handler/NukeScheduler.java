39c35a8ca1b7165f98d0033c7d51e703b25bcb2f
RagexPrince683 <yellowtoad91@gmail.com>
2026-05-25 05:07:48 -0400

    enable/disable nukes at scheduled time

--- src/main/java/com/hbm/handler/NukeScheduler.java
+++ src/main/java/com/hbm/handler/NukeScheduler.java
@@ -3,6 +3,9 @@ package com.hbm.handler;
 import com.hbm.config.GeneralConfig;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 
 public class NukeScheduler {
 
@@ -14,7 +17,7 @@ public class NukeScheduler {
 		if(event.phase != TickEvent.Phase.END)
 			return;
 
-		// only check every 5 seconds
+		// Check every 5 seconds
 		tickTimer++;
 
 		if(tickTimer < 100)
@@ -32,12 +35,51 @@ public class NukeScheduler {
 			GeneralConfig.enableNuking =
 				GeneralConfig.scheduledNukeValue;
 
+			boolean enabled = GeneralConfig.enableNuking;
+
+			MinecraftServer server =
+				MinecraftServer.getServer();
+
+			if(server != null) {
+
+				// Broadcast message
+				server.getConfigurationManager()
+					.sendChatMsg(new ChatComponentText(
+						(enabled
+							? EnumChatFormatting.DARK_RED + "☢ WARNING ☢ "
+							: EnumChatFormatting.GREEN + "☢ NOTICE ☢ ")
+							+ EnumChatFormatting.GOLD
+							+ "Nukes have been "
+							+ (enabled
+							? EnumChatFormatting.RED + "ENABLED"
+							: EnumChatFormatting.GREEN + "DISABLED")
+					));
+
+				// Play sound for everyone
+				server.getConfigurationManager()
+					.playerEntityList
+					.forEach(obj -> {
+
+						net.minecraft.entity.player.EntityPlayerMP player =
+							(net.minecraft.entity.player.EntityPlayerMP)obj;
+
+						player.worldObj.playSoundAtEntity(
+							player,
+							enabled
+								? "hbm:alarm.airRaid"
+								: "mob.wither.death",
+							50.0F, // volume
+							0.8F   // pitch
+						);
+					});
+			}
+
 			System.out.println(
 				"[HBM] Nukes automatically "
-					+ (GeneralConfig.enableNuking ? "enabled" : "disabled")
+					+ (enabled ? "enabled" : "disabled")
 			);
 
-			// clear schedule
+			// Clear schedule
 			GeneralConfig.scheduledNukeTime = null;
 			GeneralConfig.scheduledNukeValue = null;
 		}
