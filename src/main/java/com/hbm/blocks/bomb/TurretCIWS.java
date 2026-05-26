f7f959a7f9d6deb1ad66c094c1ee10ce09f5acde
RagexPrince683 <49804375+RagexPrince683@users.noreply.github.com>
2026-05-26 00:43:14 -0400

    Fix CIWS server crash from removed World method

--- src/main/java/com/hbm/blocks/bomb/TurretCIWS.java
+++ src/main/java/com/hbm/blocks/bomb/TurretCIWS.java
@@ -88,9 +88,6 @@ public class TurretCIWS extends TurretBase implements ITooltipProvider {
 	}
 
 	private void rayShot(World world, Vec3 vec, double posX, double posY, double posZ, int range, float damage, int hitPercent) {
-		List<Entity> entities = world.getLoadedEntityList();
-
-
 		for(float i = 0; i < range; i += 0.25F) {
 			double pX = posX + vec.xCoord * i;
 			double pY = posY + vec.yCoord * i;
