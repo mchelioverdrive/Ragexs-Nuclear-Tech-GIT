052a5a6fe957f6ef2963fb20d76eca5146f1f6c8
RagexPrince683 <49804375+RagexPrince683@users.noreply.github.com>
2026-05-25 23:08:44 -0400

    Rewrite CIWS turret AI to be power-gated and concise

--- src/main/java/com/hbm/tileentity/bomb/TileEntityTurretCIWS.java
+++ src/main/java/com/hbm/tileentity/bomb/TileEntityTurretCIWS.java
@@ -1,16 +1,17 @@
 package com.hbm.tileentity.bomb;
 
 import api.hbm.energymk2.IEnergyReceiverMK2;
-import com.hbm.inventory.recipes.GasCentrifugeRecipes;
+import com.hbm.blocks.turret.TurretBase;
+import com.hbm.entity.missile.EntityMissileBaseNT;
+import com.hbm.packet.TETurretPacket;
 import com.hbm.lib.Library;
-import com.hbm.main.MainRegistry;
 import com.hbm.packet.AuxGaugePacket;
 import com.hbm.packet.PacketDispatcher;
 
 import com.hbm.util.fauxpointtwelve.DirPos;
-import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Vec3;
 
 public class TileEntityTurretCIWS extends TileEntityTurretBase implements IEnergyReceiverMK2 {
 
@@ -36,37 +37,90 @@ public class TileEntityTurretCIWS extends TileEntityTurretBase implements IEnerg
 
 	@Override
 	public void updateEntity() {
-
-		if(!worldObj.isRemote) {
-			updateConnections();
-
-			if(hasPower()) {
-				this.power -= consumption;
-
-				if(this.power < 0) {
-					this.power = 0;
-				}
-			}
-
-			if(spin > 0)
-				spin -= 1;
-
-			rotation += spin;
-			rotation = rotation % 360;
-
-			PacketDispatcher.wrapper.sendToAll(
-				new AuxGaugePacket(
-					xCoord,
-					yCoord,
-					zCoord,
-					rotation,
-					0
-				)
-			);
+		if(worldObj.isRemote) {
+			return;
 		}
 
-		super.updateEntity();
+		updateConnections();
+		consumeIdlePower();
+		updateSpin();
+
+		if(!isAI || !hasPower()) {
+			use = 0;
+			return;
+		}
+
+		Entity target = findClosestMissile();
+		if(target == null) {
+			use = 0;
+			return;
+		}
+
+		aimAt(target);
+		use++;
+		if(ammo > 0 && worldObj.getBlock(xCoord, yCoord, zCoord) instanceof TurretBase) {
+			if(((TurretBase) worldObj.getBlock(xCoord, yCoord, zCoord)).executeHoldAction(worldObj, use, rotationYaw, rotationPitch, xCoord, yCoord, zCoord)) {
+				ammo--;
+			}
+		}
 	}
+
+	private void consumeIdlePower() {
+		if(hasPower()) {
+			power = Math.max(0, power - consumption);
+		}
+	}
+
+	private void updateSpin() {
+		if(spin > 0) spin--;
+		rotation = (rotation + spin) % 360;
+		PacketDispatcher.wrapper.sendToAll(new AuxGaugePacket(xCoord, yCoord, zCoord, rotation, 0));
+	}
+
+	private Entity findClosestMissile() {
+		Object[] iter = worldObj.loadedEntityList.toArray();
+		double radius = 100000;
+		Entity target = null;
+
+		for(Object obj : iter) {
+			Entity e = (Entity) obj;
+			if(!(e instanceof EntityMissileBaseNT) || !canSee(e)) continue;
+			double distance = e.getDistanceSq(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5);
+			if(distance < radius) {
+				radius = distance;
+				target = e;
+			}
+		}
+		return target;
+	}
+
+	private boolean canSee(Entity e) {
+		Vec3 turret = Vec3.createVectorHelper(xCoord + 0.5, yCoord + 1.5, zCoord + 0.5);
+		Vec3 entity = Vec3.createVectorHelper(e.posX, e.posY + e.getEyeHeight(), e.posZ);
+		Vec3 side = Vec3.createVectorHelper(entity.xCoord - turret.xCoord, entity.yCoord - turret.yCoord, entity.zCoord - turret.zCoord).normalize();
+
+		turret.xCoord += side.xCoord;
+		turret.yCoord += side.yCoord;
+		turret.zCoord += side.zCoord;
+
+		return !Library.isObstructed(worldObj, turret.xCoord, turret.yCoord, turret.zCoord, entity.xCoord, entity.yCoord, entity.zCoord);
+	}
+
+	private void aimAt(Entity target) {
+		Vec3 turret = Vec3.createVectorHelper(
+			target.posX - (xCoord + 0.5),
+			target.posY + target.getEyeHeight() - (yCoord + 1.5),
+			target.posZ - (zCoord + 0.5)
+		);
+
+		rotationPitch = -Math.asin(turret.yCoord / turret.lengthVector()) * 180 / Math.PI;
+		rotationYaw = -Math.atan2(turret.xCoord, turret.zCoord) * 180 / Math.PI;
+		rotationPitch = Math.max(-60, Math.min(30, rotationPitch));
+
+		worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+		PacketDispatcher.wrapper.sendToAll(new TETurretPacket(xCoord, yCoord, zCoord, rotationYaw, rotationPitch));
+	}
+
 
 	private DirPos[] getConPos() {
 		return new DirPos[] {
