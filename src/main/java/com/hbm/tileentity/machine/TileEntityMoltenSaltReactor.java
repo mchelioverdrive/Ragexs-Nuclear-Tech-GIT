590fae667ff5f7bb213a54b6679de21f9c265495
RagexPrince683 <yellowtoad91@gmail.com>
2026-06-01 15:22:58 -0400

    that shit doesn't exist anymore

--- src/main/java/com/hbm/tileentity/machine/TileEntityMoltenSaltReactor.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityMoltenSaltReactor.java
@@ -101,6 +101,7 @@ public class TileEntityMoltenSaltReactor extends TileEntityMachineBase implement
 
 	public boolean isShielded() {
 		for(ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
+			//if it's completely covered up, how are we supposed to get fluid in or out?
 			Block block = worldObj.getBlock(xCoord + dir.offsetX, yCoord + dir.offsetY, zCoord + dir.offsetZ);
 			if(!isShieldBlock(block)) return false;
 		}
@@ -111,7 +112,7 @@ public class TileEntityMoltenSaltReactor extends TileEntityMachineBase implement
 		return block instanceof IRadResistantBlock ||
 				block == ModBlocks.block_lead ||
 				block == ModBlocks.block_boron ||
-				block == ModBlocks.concrete_super ||
+				block == ModBlocks.brick_concrete ||
 				block == ModBlocks.concrete_asbestos;
 	}
 
