cd364b42a35535f13911ed3c55e80298095ec698
RagexPrince683 <yellowtoad91@gmail.com>
2026-07-19 23:54:11 -0400

    fix some stuff

--- src/main/java/com/hbm/blocks/machine/MachineTurbineGas.java
+++ src/main/java/com/hbm/blocks/machine/MachineTurbineGas.java
@@ -121,7 +121,7 @@ public class MachineTurbineGas extends BlockDummyable implements ILookOverlay, I
 	}
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean ext) {
-		this.addStandardInfo(stack, player, list, ext);
+		//this.addStandardInfo(stack, player, list, ext);
 		list.add(EnumChatFormatting.DARK_RED + I18nUtil.resolveKey("tooltip.furnace.monoxide"));
 	}
 }
