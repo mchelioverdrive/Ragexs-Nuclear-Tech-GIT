05f405197e3538e99a21c8bbc12779dc655d2760
RagexPrince683 <yellowtoad91@gmail.com>
2026-07-27 13:05:18 -0400

    Update Fluids.java

--- src/main/java/com/hbm/inventory/fluid/Fluids.java
+++ src/main/java/com/hbm/inventory/fluid/Fluids.java
@@ -582,7 +582,7 @@ public class Fluids {
 		KRYPTON_FLUORINE = new FluidType("KRYPTON_FLUORINE", 0xB9B9B9, 2, 0, 1, EnumSymbol.NONE).addTraits(GASEOUS, NOCON);
 		XENON_CHLORINE = new FluidType("XENON_CHLORINE", 0xB9B9B9, 2, 0, 1, EnumSymbol.NONE).addTraits(GASEOUS, NOCON);
 		HELIUM4_HOT =			new FluidType("HELIUM4_HOT", 0x99525E, 1, 0, 0, EnumSymbol.NONE).setTemp(750).addTraits(GASEOUS);
-		ROCKET_FUEL =			new FluidType("ROCKET_FUEL",		0x74633B, 1, 2, 0, EnumSymbol.NONE).addContainers(new CD_Canister(0x74633B)).addTraits(new FT_Flammable(300_000), LIQUID);
+		ROCKET_FUEL =			new FluidType("ROCKET_FUEL",		0x74633B, 1, 2, 0, EnumSymbol.NONE).addContainers(new CD_Canister(0x74633B)).addTraits(new FT_Flammable(300_000), LIQUID, new FT_Rocket(270, 700_000));
 
 //I am getting really sick and tired of this retarded ass fluid system
 
