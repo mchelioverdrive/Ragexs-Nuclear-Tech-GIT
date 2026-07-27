533d5a0696acc7582b8386f0e85cd98f36ad898d
RagexPrince683 <yellowtoad91@gmail.com>
2026-07-27 14:09:15 -0400

    Update SolderingRecipes.java

--- src/main/java/com/hbm/inventory/recipes/SolderingRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/SolderingRecipes.java
@@ -263,6 +263,7 @@ public class SolderingRecipes extends SerializableRecipe {
 						new OreDictStack(PB.wireFine(), 24)}
 		));
 		recipes.add(new SolderingRecipe(new ItemStack(ModItems.circuit, 1, EnumCircuitType.CONTROLLER_QUANTUM.ordinal()), 600, 250_000,
+				new FluidStack(Fluids.HELIUM3, 15_000),
 				new AStack[] {
 						new ComparableStack(ModItems.circuit, lbsm ? 8 : 16, EnumCircuitType.CHIP_QUANTUM),
 						new ComparableStack(ModItems.circuit, lbsm ? 16 : 48, EnumCircuitType.CHIP_BISMOID),
@@ -272,6 +273,7 @@ public class SolderingRecipes extends SerializableRecipe {
 						new ComparableStack(ModItems.upgrade_overdrive_1)},
 				new AStack[] {
 						new OreDictStack(PB.wireFine(), 32)}
+
 		));
 
 
