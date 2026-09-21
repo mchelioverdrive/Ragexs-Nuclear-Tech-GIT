2476a096d374e64ff99aac82ccbc44e204cc9fc2
RagexPrince683 <yellowtoad91@gmail.com>
2026-09-20 22:35:01 -0400

    Update Fluids.java

--- src/main/java/com/hbm/inventory/fluid/Fluids.java
+++ src/main/java/com/hbm/inventory/fluid/Fluids.java
@@ -586,10 +586,10 @@ public class Fluids {
 		XENON_CHLORINE = new FluidType("XENON_CHLORINE", 0xB9B9B9, 2, 0, 1, EnumSymbol.NONE).addTraits(GASEOUS, NOCON);
 		HELIUM4_HOT =			new FluidType("HELIUM4_HOT", 0x99525E, 1, 0, 0, EnumSymbol.NONE).setTemp(750).addTraits(GASEOUS);
 		ROCKET_FUEL =			new FluidType("ROCKET_FUEL",		0x74633B, 1, 2, 0, EnumSymbol.NONE).addContainers(new CD_Canister(0x74633B)).addTraits(new FT_Flammable(300_000), LIQUID, new FT_Rocket(270, 700_000));
-		FRESH_WATER = new FluidType("FRESH_WATER", 0x3A7DFF, 1, 0, 0, EnumSymbol.NONE).addTraits(LIQUID);
-		LIGHT_WATER = new FluidType("LIGHT_WATER", 0x3A7DFF, 1, 0, 0, EnumSymbol.NONE).addTraits(LIQUID);
-		BORATED_WATER = new FluidType("BORATED_WATER", 0x3A7DFF, 1, 0, 0, EnumSymbol.NONE).addTraits(LIQUID);
-
+		FRESH_WATER = new FluidType("FRESH_WATER", 0x55BDEB, 1, 0, 0, EnumSymbol.NONE).addTraits(LIQUID);
+		LIGHT_WATER = new FluidType("LIGHT_WATER", 0xBDEBFF, 1, 0, 0, EnumSymbol.NONE).addTraits(LIQUID);
+		BORATED_WATER = new FluidType("BORATED_WATER", 0x63C7B2, 1, 0, 0, EnumSymbol.NONE).addTraits(LIQUID);
+		
 //I am getting really sick and tired of this retarded ass fluid system
 
 
